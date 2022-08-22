db9ae72da66ec83209a0d7a3c27a38d9789e4d39
Ethan Davis <etdavis@google.com>
2022-08-22 15:17:06 -0700

    add binary

--- main.go
+++ main.go
@@ -6,5 +6,17 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hello world at %s! V2\n", time.Now().Local().Format("Jan 2, 2006 at 3:04:05 PM"))
+	// fmt.Printf("Hello world at %s! V2\n", time.Now().Local().Format("Jan 2, 2006 at 3:04:05 PM"))
+
+	now := time.Now()
+
+	fmt.Println(now)
+
+	tok, err := time.LoadLocation("Asia/Tokyo")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(now.In(tok).Weekday())
 }
